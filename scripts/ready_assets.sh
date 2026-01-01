45255624b1ea14985ec266f34061c59bf0a4f367
mammad <mammad@email.com>
2026-01-01 12:37:23 +0000

    assets

---
+++ scripts/ready_assets.sh
@@ -0,0 +1,6 @@
+cp /content/drive/MyDrive/BigData/hw3/asset/Homo_sapiens_assembly38.fasta.gz* . 
+cp /content/drive/MyDrive/BigData/hw3/asset/hashTable . 
+cp /content/drive/MyDrive/BigData/hw3/asset/large_1.fastq.gz .
+cp /content/drive/MyDrive/BigData/hw3/asset/large_2.fastq.gz .
+mkdir -p kmers_index 
+cp hashTable kmers_index/ 
