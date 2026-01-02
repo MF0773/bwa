78e2d633d0712c2bbcd4cb5f208c3a4dd09ec603
mammad <mammad@email.com>
2026-01-02 18:55:42 +0000

    small fastq.gz

--- scripts/ready_assets.sh
+++ scripts/ready_assets.sh
@@ -4,3 +4,7 @@ cp /content/drive/MyDrive/BigData/hw3/asset/large_1.fastq.gz .
 cp /content/drive/MyDrive/BigData/hw3/asset/large_2.fastq.gz .
 mkdir -p kmers_index 
 mv hashTable kmers_index/ 
+
+N=50000
+zcat large_1.fastq.gz | head -n $((N*4)) | gzip > small_1.fastq.gz
+zcat large_2.fastq.gz | head -n $((N*4)) | gzip > small_2.fastq.gz
